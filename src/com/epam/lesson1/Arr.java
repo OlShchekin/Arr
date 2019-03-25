e42f9ff0961fb83a1fe43d5e4c65c02cd16d5c26
OlShchekin <olegshchekin09@gmail.com>
2019-03-25 09:28:05 +0200

    Initial commit

---
+++ src/com/epam/lesson1/Arr.java
@@ -0,0 +1,10 @@
+package com.epam.lesson1;
+
+public class Arr {
+    public static void main(String[] args) {
+        int num = 20;
+        int invers = ~num;
+        int revers = ++invers;
+        System.out.println(revers);
+    }
+}
